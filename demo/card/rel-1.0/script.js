d2a1c927d50ef4f9114bbd2ea2044da1348bae65
Suapsk <88731065+suapsk@users.noreply.github.com>
2021-10-07 22:27:12 +0330

    Update script.js

--- demo/card/rel-1.0/script.js
+++ demo/card/rel-1.0/script.js
@@ -7,5 +7,5 @@ if (window.screen.width =< 500) {
 		document.getElementById("main").style.width = "100%;";
 }
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test( navigator.userAgent )){
-    document.getElementById('up_arrow').style.width = '100px';
+    document.getElementById('main').style.width = '100px';
 }
