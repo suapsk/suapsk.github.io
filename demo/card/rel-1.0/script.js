3e49aa75642ea8f84bff795cfc3d1f503029d27d
Suapsk <88731065+suapsk@users.noreply.github.com>
2021-10-07 22:24:42 +0330

    Add files via upload

---
+++ demo/card/rel-1.0/script.js
@@ -0,0 +1,11 @@
+function mainwidth() {
+	if (window.screen.width =< 500) {
+		document.getElementById("main").style.width = "100%;";
+	}
+}
+if (window.screen.width =< 500) {
+		document.getElementById("main").style.width = "100%;";
+}
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test( navigator.userAgent )){
+    document.getElementById('up_arrow').style.width = '100%';
+}
