6af17017ff5191a00f62857524e5509af603cb9d
suapsk <88731065+suapsk@users.noreply.github.com>
2021-11-03 16:56:44 -0500

    Update script.js

--- temp/seed/assets/script.js
+++ temp/seed/assets/script.js
@@ -4,33 +4,42 @@ function getTime() {
 	var t = new Date();
     var sec = t.getSeconds();
     var msec = t.getMilliseconds();
+    var min = t.getMinutes();
+    var hr = t.getHours();
+    var d = t.getDate();
+    var m = t.getMonth();
+    var y = t.getFullYear();
+    const arg0 = m + d + hr + min + sec + msec;
+    const arg1 = y - sec;
+    const num0 = y * arg0 + arg1 ;
+    const arg2 = y * num0;
+    const num1 = y + arg2 + 1;
+    const num2 = num1 - sec * y;
+    const arg3 = sec * msec * arg0 + 1;
+    const timehash = num2 * arg3 + 1;
     const locate = document.getElementById("timehash");
-    const num0 = sec;
-    const num1 = msec;
-    const num2 = sec + msec;
-    const num3 = msec - sec;
-    const num4 = sec * msec;
-    const hash0 = num2  + num1 + num3 + num4 + num4 + num3 + num0 * num1 * num2 * num4;
-    const hash1 = num2 * num0 +  num1 + num3 + num4 + num4 + num3 * num4 + num4 * num2;
-    const timehash = hash0 + hash1 * hash0 + 1;
     locate.innerHTML = timehash;
 }
 setInterval(getTime, 100);
-
 
 function generateSeed() {
 	var t = new Date();
     var sec = t.getSeconds();
     var msec = t.getMilliseconds();
+    var min = t.getMinutes();
+    var hr = t.getHours();
+    var d = t.getDate();
+    var m = t.getMonth();
+    var y = t.getFullYear();
+    const arg0 = m + d + hr + min + sec + msec;
+    const arg1 = y - sec;
+    const num0 = y * arg0 + arg1 ;
+    const arg2 = y * num0;
+    const num1 = y + arg2 + 1;
+    const num2 = num1 - sec * y;
+    const arg3 = sec * msec * arg0 + 1;
+    const seed = num2 * arg3 + 1;
     const locate = document.getElementById("uid_19");
-    const num0 = sec;
-    const num1 = msec;
-    const num2 = sec + msec;
-    const num3 = msec - sec;
-    const num4 = sec * msec;
-    const hash0 = num2  + num2 + num3 + num4 + num4 + num3 + num0 * num1 * num2 * num4;
-    const hash1 = num2 * num0 +  num1 + num3 + num4 + num4 + num3 * num4 + num4 * num2;
-    const seed = hash0 + hash1 * hash0 + 1;
 	locate.value = seed;
 }
 
@@ -38,28 +47,3 @@ function copyseed() {
 	const seedvalue = document.getElementById('uid_19').value;
 	navigator.clipboard.writeText(seedvalue);
 }
-
-
-
-
-
-
-
-
-
-
-
- /*
-return `${Math.floor(Math.random() * 0*ffffffffffffffff)
-		.toString(16)
-		.padEnd(16, "0")}`;
-
-
-
-
-
-
-
-
-
- */
